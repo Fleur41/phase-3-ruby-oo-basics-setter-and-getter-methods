77be315fbbabd33afa7cfce66f3a3ba5ffbe90ca
Fleur41 <Muriithikings456@gmail.com>
2023-02-13 12:41:08 +0300

    Completed lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+    def name=(person)
+        @name = person
+    end
 
+    def name
+        @name
+    end
+
+    def job=(work)
+        @job = work
+    end
+
+    def job
+        @job
+    end
 end
